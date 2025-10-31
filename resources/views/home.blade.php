be2831568a6386b155c67fa812de6b8dee1f5a5a
HussainALAradi5 <hussainaradi.ha@gmail.com>
2025-10-31 13:46:44 +0300

    some minor naming convension also some minor changes such that now if we  press the hospital system we will back to the home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>Welcome to the Hospital Management System</h1>
+        <p class="lead">Manage countries, medicines, and user profiles with ease.</p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use App\Http\Controllers\UserAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layouts.app');
-});
+    return view('home');
+})->name('home');
 
 
 
